a20a77c79e678d26623cf702bec4e0f4ba46e443
Tatiana <tburbano2@gmail.com>
2025-09-16 14:15:59 -0500

    or

--- start/routes/GestionEppRoutes.ts
+++ start/routes/GestionEppRoutes.ts
@@ -1,26 +1,9 @@
 import Route from '@adonisjs/core/services/router'
-import CargosController from '#controller/CargosController'
+import GestionEppController from '../../app/controller/GestionEppController.js'
+const gestionEppController = new GestionEppController()
 
-// Ruta pública: listar cargos
-Route.get('/listarCargos', 'CargosController.listarCargos')
-// Ruta privada: crear cargo
-Route.post('/crearCargo', 'CargosController.crearCargo')
-
-// Ruta privada: actualizar cargo
-Route.put('/actualizarCargo/:id', 'CargosController.actualizarCargo')
-
-// Ruta privada: eliminar cargo
-Route.delete('/eliminarCargo/:id', 'CargosController.eliminarCargo')
-
-// Nota: middleware se puede aplicar a nivel de controlador o ruta
-// Ruta privada: listar cargos activos
-Route.get('/listarCargosActivos', 'CargosController.listarCargosActivos')
-
-// Ruta privada: listar cargos inactivos
-Route.get('/listarCargosInactivos', 'CargosController.listarCargosInactivos')
-
-// Ruta privada: listar cargo por ID
-Route.get('/listarCargo/:id', 'CargosController.listarCargo')
-
-// Ruta privada: conteo de cargos
-Route.get('/conteoCargos', 'CargosController.conteoCargos')
+Route.post('/crearGestionEpp', gestionEppController.crear)
+Route.get('/listarGestiones', gestionEppController.listar)
+Route.get('/mostrarGestion/:id', gestionEppController.mostrar)
+Route.put('/actualizarGestion/:id', gestionEppController.actualizar)
+Route.delete('/eliminarGestion/:id', gestionEppController.eliminar)
